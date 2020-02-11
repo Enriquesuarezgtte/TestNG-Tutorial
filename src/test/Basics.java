a66ce37f16ca9d9d88cb42a855c4b713e2bf3127
Enrique Suárez Guette <36301561+Enriquesuarezgtte@users.noreply.github.com>
2020-02-11 14:54:27 -0500

    Basics (#1)
    * basics hints to start with testNG
    
    * README edited
    
    * whitespace

---
+++ src/test/Basics.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class Basics {
+	
+	@Test
+	public void Demo() {
+		System.out.println("Hello world");
+	}
+	
+	@Test
+	public void Demo2() {
+		System.out.println("hello again");
+	}
+
+}
