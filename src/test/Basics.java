c7ad0854beeb856a97b4d4074e2202722868bd30
Enrique Suárez Guette <36301561+Enriquesuarezgtte@users.noreply.github.com>
2020-02-14 18:58:11 -0500

    Annotation (#3)
    * exclude-include tests
    
    * deleted comments
    
    * deleted whitespaces and comments
    
    * annotation
    
    * deleted whitespaces
    
    * annotations
    
    * added EOF

--- src/test/Basics.java
+++ src/test/Basics.java
@@ -1,22 +1,70 @@
 package test;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Basics {
-	
-	@Test
-	public void Demo() {
-		System.out.println("Hello world");
-	}
-	
-	@Test
-	public void Demo2() {
-		System.out.println("hello again");
-	}
 
-	@Test
-	public void NoDemo2() {
-		System.out.println("hello again");
-	}
+  @BeforeClass
+  public void beforeClass() {
+    System.out.println("Before class ");
+  }
+  
+  @AfterClass
+  public void afterClass() {
+    System.out.println("After class ");
+  }
+
+  @Test
+  public void Demo() {
+    System.out.println("Hello world");
+  }
+
+  @Test
+  public void Demo2() {
+    System.out.println("hello again");
+  }
+
+  @BeforeTest
+  public void cleanData() {
+    System.out.println("Before Test");
+  }
+
+  @BeforeSuite
+  public void cleanDataSuite() {
+    System.out.println("Before suite");
+  }
+
+  @AfterSuite
+  public void cleanDataSuiteAfter() {
+    System.out.println("After suite");
+  }
+
+  @AfterTest
+  public void cleanAfter() {
+    System.out.println("After Test");
+  }
+
+  @BeforeMethod
+  public void beforeMethod() {
+    System.out.println("Before Method");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+    System.out.println("After Method");
+  }
+
+  @Test
+  public void NoDemo2() {
+    System.out.println("hello again");
+  }
 
 }
