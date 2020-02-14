f2410748654479a954bddd6f4e9dd1053e97a1bc
Enrique Suarez <enriquesuarezgtte@gmail.com>
2020-02-14 18:43:31 -0500

    group

--- src/test/LoginTest.java
+++ src/test/LoginTest.java
@@ -4,12 +4,13 @@ import org.testng.annotations.Test;
 
 public class LoginTest {
 	
-	@Test
-	public void webLoginCarLoan() {
+	@Test(groups= {"Smoke"}, timeOut = 400)
+	public void webLoginCarLoan() throws InterruptedException {
+	  Thread.sleep(350);
 		System.out.println("weblogin");
 	}
 	
-	@Test
+	@Test(groups= {"Smoke"}, dependsOnMethods = {"webLoginCarLoan"}, enabled = true)
 	public void mobileLoginCarLoan(){
 		System.out.println("mobileLogin");
 	}
