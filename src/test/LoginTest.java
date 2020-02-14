2874cd68c250f97781f71a3e528b5f16c1c18af1
Enrique Suárez Guette <36301561+Enriquesuarezgtte@users.noreply.github.com>
2020-02-14 18:31:21 -0500

    Exclude include (#2)
    * exclude-include tests
    
    * deleted comments
    
    * deleted whitespaces and comments
    
    * deleted whitespaces

---
+++ src/test/LoginTest.java
@@ -0,0 +1,22 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void webLoginCarLoan() {
+		System.out.println("weblogin");
+	}
+	
+	@Test
+	public void mobileLoginCarLoan(){
+		System.out.println("mobileLogin");
+	}
+	
+	@Test
+	public void loginAPICarLoan() {
+		System.out.println("APIlogin");
+	}
+
+}
